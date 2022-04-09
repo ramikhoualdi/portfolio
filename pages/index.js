c5ad3e3f962a9838cdfd60af037ae73360117e3c
rami khoualdi <ramykhweldi@gmail.com>
2022-04-09 19:40:21 +0100

    forth commit

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Ramy Khoualdi" />
+        <title>ramykhoualdi.com</title>
+        <meta name="description" content="Ramy Khoualdi Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Portfolio styles={styles} />
