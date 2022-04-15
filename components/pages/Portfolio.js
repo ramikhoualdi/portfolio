f650f97dcdece5aebf3a61ce6b49dc46a375fee9
rami khoualdi <ramykhweldi@gmail.com>
2022-04-15 20:53:39 +0100

    section one Done MQ added AW

--- components/pages/Portfolio.js
+++ components/pages/Portfolio.js
@@ -1,48 +1,40 @@
-import React, { useRef } from "react";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import React from "react";
 
 const Portfolio = (props) => {
-  const { styles } = props;
-  const containerRef = useRef(null);
+  const { styles, scroll } = props;
+
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-      }}
-      containerRef={containerRef}
-    >
-      <main className={styles.main} data-scroll-section>
-        {/* Section 1 */}
-        <div className={styles.heading}>
-          <div className={styles.subheading}>
-            <p className={styles.slideup}>
-              <span
-                className={styles.title1}
-                data-scroll
-                data-scroll-speed="3"
-                data-scroll-position="top"
-                data-scroll-delay="0.05"
-              >
-                Ramy Khoualdi
-              </span>
-            </p>
-          </div>
-          <div className={styles.subheading}>
-            <p className={styles.slideup2}>
-              <span
-                className={styles.title2}
-                data-scroll
-                data-scroll-speed="6"
-                data-scroll-position="top"
-                data-scroll-delay="0.05"
-              >
-                Frontend Developer
-              </span>
-            </p>
-          </div>
+    <main className={styles.main} data-scroll-section>
+      {/* Section 1 */}
+      <div className={styles.heading}>
+        <div className={styles.subheading}>
+          <p className={styles.slideup}>
+            <span
+              className={styles.title1}
+              data-scroll
+              data-scroll-speed="3"
+              data-scroll-position="top"
+              data-scroll-delay="0.05"
+            >
+              Ramy Khoualdi
+            </span>
+          </p>
         </div>
-      </main>
-    </LocomotiveScrollProvider>
+        <div className={styles.subheading}>
+          <p className={styles.slideup2}>
+            <span
+              className={styles.title2}
+              data-scroll
+              data-scroll-speed="6"
+              data-scroll-position="top"
+              data-scroll-delay="0.05"
+            >
+              Frontend Developer
+            </span>
+          </p>
+        </div>
+      </div>
+    </main>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import { useRef } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Portfolio from "../components/pages/Portfolio";
+import { useRouter } from "next/router";
+import {
+  LocomotiveScrollProvider,
+  useLocomotiveScroll,
+} from "react-locomotive-scroll";
 
 export default function Home() {
+  const { scroll } = useLocomotiveScroll();
+  const { asPath } = useRouter();
+  const containerRef = useRef(null);
   return (
     <div className={styles.container}>
       <Head>
@@ -10,7 +19,21 @@ export default function Home() {
         <meta name="description" content="Ramy Khoualdi Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Portfolio styles={styles} />
+      <LocomotiveScrollProvider
+        options={{
+          smooth: true,
+        }}
+        watch={[asPath, Portfolio]}
+        location={asPath}
+        containerRef={containerRef}
+        onLocationChange={(scroll) =>
+          scroll.scrollTo(0, { duration: 0, disableLerp: true })
+        }
+      >
+        <main data-scroll-container ref={containerRef}>
+          <Portfolio styles={styles} scroll={scroll} />
+        </main>
+      </LocomotiveScrollProvider>
     </div>
   );
 }
