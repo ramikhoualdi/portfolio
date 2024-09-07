38cdbde36c072ee5cdcb25be846870aaa138d7fd
Rami khoualdi <rami.khoualdi@credium.de>
2024-09-07 01:21:24 +0100

    adjust main layout

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,13 +1,15 @@
-import FeaturedWork from "@/components/content/featured-work";
 import Me from "@/components/content/me";
 import MobileApps from "@/components/content/mobile-apps";
-import Testimonials from "@/components/content/testimonials";
+import { WebApps } from "@/components/content/web-apps";
 
 export default function IndexPage() {
   return (
-    <div className="relative px-14">
-      <Me />
-      {/* <MobileApps /> */}
+    <div className="relative">
+      <div className="px-20">
+        <Me />
+      </div>
+      <WebApps />
+      <MobileApps />
       {/* <FeaturedWork /> */}
       {/* <Testimonials /> */}
       {/* <div className="min-h-[2000px] w-screen" /> */}
