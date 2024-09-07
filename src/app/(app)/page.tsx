2545e099fd50701795230f0b8066257d24c06616
Rami khoualdi <rami.khoualdi@credium.de>
2024-09-07 18:03:29 +0100

    updaet hero section

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,3 +1,4 @@
+import AboutMe from "@/components/content/about-me";
 import Me from "@/components/content/me";
 import MobileApps from "@/components/content/mobile-apps";
 import { WebApps } from "@/components/content/web-apps";
@@ -5,11 +6,12 @@ import { WebApps } from "@/components/content/web-apps";
 export default function IndexPage() {
   return (
     <div className="relative">
-      <div className="px-20">
+      <div className="px-4 lg:px-20">
         <Me />
       </div>
-      <WebApps />
-      <MobileApps />
+      <AboutMe />
+      {/* <WebApps /> */}
+      {/* <MobileApps /> */}
       {/* <FeaturedWork /> */}
       {/* <Testimonials /> */}
       {/* <div className="min-h-[2000px] w-screen" /> */}
--- src/components/content/me.tsx
+++ src/components/content/me.tsx
@@ -2,28 +2,27 @@ import { FadeText } from "@/components/magicui/fade-text";
 
 export default function Me() {
   return (
-    <div className="flex h-dvh w-full flex-col items-center justify-center">
-      <FadeText
-        className="xs:text-[10rem] font-['Valencia'] text-[4rem] leading-[14rem] md:text-[8rem] lg:text-[10rem] xl:text-[12rem] 2xl:text-[16rem]"
-        direction="down"
-        framerProps={{
-          show: { transition: { delay: 0.6 } },
-        }}
-        text="Rami Khoualdi"
-      />
-      <FadeText
-        className="text-4xl font-normal"
-        direction="left"
-        framerProps={{
-          show: { transition: { delay: 0.7 } },
-        }}
-        text="Frontend Engineer"
-      />
-      <p>
-        As a skilled Full-Stack Developer specializing in Reactjs and React
-        Native, I have a proven track record of crafting exceptional web and
-        mobile apps with top-notch user experience and responsive design.
-      </p>
+    <div className="flex min-h-dvh w-full flex-col items-start justify-center md:items-center">
+      <div className="mb-[20px]">
+        <FadeText
+          className="font-['Valencia'] text-[9rem] leading-[140px] md:text-[9rem] lg:text-[12rem]"
+          direction="down"
+          framerProps={{
+            show: { transition: { delay: 0.6 } },
+          }}
+          text="Rami Khoualdi"
+        />
+      </div>
+      <div className="w-full text-center">
+        <FadeText
+          className="text-xl font-extralight md:text-2xl lg:text-4xl"
+          direction="left"
+          framerProps={{
+            show: { transition: { delay: 0.7 } },
+          }}
+          text="Software Engineer"
+        />
+      </div>
     </div>
   );
 }
