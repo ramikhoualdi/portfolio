b047aa9cd1ef0eb4deb13895880328bb915028cb
Rami khoualdi <rami.khoualdi@credium.de>
2024-09-07 01:21:41 +0100

    add web and mobile apps

--- src/components/content/web-apps.tsx
+++ src/components/content/web-apps.tsx
@@ -3,18 +3,11 @@
 import React from "react";
 import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
 
-export default function MobileApps() {
-  const appNames = [
-    "sidegame",
-    "medipocket",
-    // "rideshare",
-    // "hearppl",
-    // "ertnashville",
-    // "d2",
-  ];
-  const imageCount = 5;
+export function WebApps() {
+  const appNames = ["esg-suite"];
+  const imageCount = 12;
 
-  const mobileAppsImages = appNames.map((appName) =>
+  const webAppsImages = appNames.map((appName) =>
     Array.from(
       { length: imageCount },
       (_, i) => `/images/${appName}/s${i + 1}.png`,
@@ -28,11 +21,11 @@ export default function MobileApps() {
         index={index}
         cardImg={card}
         layout={true}
-        isMobile={true}
+        isMobile={false}
       />
     ));
 
-  const allCards = mobileAppsImages.map(createCards);
+  const allCards = webAppsImages.map(createCards);
 
   const cardSets = Object.fromEntries(
     appNames.map((name, index) => [`${name}Cards`, allCards[index]]),
@@ -43,7 +36,7 @@ export default function MobileApps() {
   return (
     <div className="h-full w-full py-20">
       <h2 className="max-w-7xl pl-12 font-sans text-xl font-bold text-neutral-800 dark:text-neutral-200 md:text-5xl">
-        IOS - Android
+        Web
       </h2>
       {entries.map(([appName, cards]) => (
         <Carousel key={appName} items={cards} />
