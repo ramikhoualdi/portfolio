e21a3da058f122138c7084f3102e4161eeaf9cbb
Rami khoualdi <rami.khoualdi@credium.de>
2024-09-11 00:00:19 +0100

    shuffle array utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,5 +6,14 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function absoluteUrl(path: string) {
-  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`
+  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
 }
+
+export const shuffleArray = (array: string[]): string[] => {
+  const shuffled = [...array]; // Make a copy to avoid mutating the original
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap
+  }
+  return shuffled;
+};
