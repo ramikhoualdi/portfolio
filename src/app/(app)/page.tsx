cb0c35dd524dc8fb728a23aaa151680b6686b550
Rami khoualdi <rami.khoualdi@credium.de>
2024-09-10 23:59:54 +0100

    refactor main page to tsx

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import AboutMe from "@/components/content/about-me";
+import { BroadcastIndicator } from "@/components/content/broadcast-indicator";
+import Me from "@/components/content/me";
+import MobileApps from "@/components/content/mobile-apps";
+import { UsersCursors } from "@/components/content/users-cursors";
+import { WebApps } from "@/components/content/web-apps";
+import { FollowerPointerCard } from "@/components/ui/following-pointer";
+import { usePortfolio } from "@/context/portfolio-context";
+
+export default function IndexPage() {
+  const { state } = usePortfolio();
+  const { currentUserLocation } = state;
+  
+  return (
+    <FollowerPointerCard
+      title={
+        currentUserLocation?.city && currentUserLocation?.city?.length > 0
+          ? `You(${currentUserLocation?.city + ", " + currentUserLocation?.country})`
+          : "You"
+      }
+    >
+      <div className="max-w-screen relative">
+        <div className="px-4 lg:px-20">
+          <Me />
+        </div>
+        <AboutMe />
+        <WebApps />
+        <MobileApps />
+        {/* <FeaturedWork /> */}
+        {/* <Testimonials /> */}
+        <BroadcastIndicator />
+        <UsersCursors />
+      </div>
+    </FollowerPointerCard>
+  );
+}
